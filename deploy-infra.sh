39270d16c1cde9f69d0d4ab7eb2700129ec35a47
Billy <billy>
2020-12-02 21:30:47 +0000

    Add codebuild / codedeploy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \ --query "Account" --output text`
-CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
-
-
 STACK_NAME=awsbootstrap
 REGION=eu-west-1
 CLI_PROFILE=awsbootstrap
 
 EC2_INSTANCE_TYPE=t2.micro
 
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+  --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+
+
+# Generate a personal access token with repo and admin:repo_hook
+# permissions from https://github.com/settings/tokens
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
+
 echo -e "\n\n=========== Deploying setup.yml ==========="
 aws cloudformation deploy \
---region $REGION \
---profile $CLI_PROFILE \
---stack-name $STACK_NAME-setup \
---template-file setup.yml \
---no-fail-on-empty-changeset \
---capabilities CAPABILITY_NAMED_IAM \
---parameter-overrides \
-CodePipelineBucket=$CODEPIPELINE_BUCKET
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+  CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
@@ -31,10 +40,17 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-  EC2InstanceType=$EC2_INSTANCE_TYPE
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 if [ $? -eq 0 ]; then
 aws cloudformation list-exports \
---profile awsbootstrap \
---query "Exports[?Name=='InstanceEndpoint'].Value"
+  --profile awsbootstrap \
+  --query "Exports[?Name=='InstanceEndpoint'].Value"
 fi
+
+read -p "Press any key to continue" x
