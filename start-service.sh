5ee1fcbc6211951c62b1f27205b00eeebc2b8941
Billy <billy>
2020-12-14 20:54:18 +0000

    Updated start-service.sh to work for IMDSv2

--- start-service.sh
+++ start-service.sh
@@ -2,9 +2,11 @@
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
 # Query the EC2 metadata service for this instance's region
-REGION=`curl -s http://169.254.169.254/latest/dynamic/instanceidentity/document | jq .region -r`
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` \
+REGION=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r`
+
 # Query the EC2 metadata service for this instance's instance-id
-export INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instanceid`
+export INSTANCE_ID=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/dynamic/instance-identity/document | jq .instanceId -r`
 # Query EC2 describeTags method and pull our the CFN Logical ID for this instance
 export STACK_NAME=`aws --region $REGION ec2 describe-tags \
 --filters "Name=resource-id,Values=${INSTANCE_ID}" \
