44b227ec9ddf71105197c806bc5df509792ba420
Billy <billy>
2020-12-28 18:37:05 +0000

    Add HTTPS listener; Add cert to launch script

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -5,6 +5,8 @@ REGION=eu-west-1
 CLI_PROFILE=awsbootstrap
 EC2_INSTANCE_TYPE=t2.micro
 DOMAIN=billy-addington.com
+CERT=`aws acm list-certificates --region $REGION --profile awsbootstrap --output text \
+        --query "CertificateSummaryList[?DomainName=='$DOMAIN'].CertificateArn | [0]"`
 
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
   --query "Account" --output text`
@@ -61,6 +63,7 @@ aws cloudformation deploy \
   --parameter-overrides \
     EC2InstanceType=$EC2_INSTANCE_TYPE \
     Domain=$DOMAIN \
+    Certificate=$CERT \
     GitHubOwner=$GH_OWNER \
     GitHubRepo=$GH_REPO \
     GitHubBranch=$GH_BRANCH \
